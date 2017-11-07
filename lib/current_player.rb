0874bf3e588886a1779e6ca7d4dda1c26fef2aa6
Remmy Clay <remmy.lauren@gmail.com>
2017-11-07 03:09:30 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -10,9 +10,8 @@ end
 
 def current_player(board)
   counter = turn_count(board)
-  if counter % 2 == 0
+  counter % 2 == 0
     "X"
-  else counter % 2 != 0
+  counter % 2 != 0
     "O"
-  end
 end
